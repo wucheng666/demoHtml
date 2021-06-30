f1218ab9bedf75f973847640726422a65776a859
吴成 <wubaodtc@sina.com>
2021-06-30 19:29:42 +0800

    Update index.js

--- sharedworker2/index.js
+++ sharedworker2/index.js
@@ -2,6 +2,9 @@ var worker = new SharedWorker("jsworker.js");
 
 function initJsWorker() {
 	worker.port.addEventListener("message", function (e) {
+		let divS = document.createElement("div");
+		divS.innerText = 'Waiting response: ' + e.data;
+		document.body.append(divS)
 		console.log('Waiting response: ' + e.data);
 	}, false);
 
