30064ff29f8924a91d7f452403d7655aa47ef9be
吴成 <wubaodtc@sina.com>
2021-06-30 19:06:59 +0800

    Update jsworker.js

--- sharedworker2/jsworker.js
+++ sharedworker2/jsworker.js
@@ -1,8 +1,10 @@
 self.addEventListener("connect", function (e) {
+		console.log("jsworker connect :", e)
 
 	var port = e.ports[0];
 
 	port.addEventListener("message", function (e) {
+		console.log("jsworker message:", e)
 		port.postMessage("Service says: " + e.data);
 	}, false);
 
