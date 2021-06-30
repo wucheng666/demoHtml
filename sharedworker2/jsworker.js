1a616fcac7c213398ad8f16b7dc221cb5ea62a7a
吴成 <wubaodtc@sina.com>
2021-06-30 19:48:01 +0800

    Update jsworker.js

--- sharedworker2/jsworker.js
+++ sharedworker2/jsworker.js
@@ -1,3 +1,4 @@
+let data = "链接成功"
 self.addEventListener("connect", function (e) {
 		console.log("jsworker connect :", e)
 
@@ -6,6 +7,18 @@ self.addEventListener("connect", function (e) {
 	port.addEventListener("message", function (e) {
 		console.log("jsworker message:", e)
 		port.postMessage("Service says2: " + e.data);
+		// 如果消息内容为空，
+		// 说明该客户端想获取共享的数据 data
+		if (e.data === "") {
+		    // 就给当前客户端发送 data 数据
+		    port.postMessage(data);
+		} else {
+		    // 否则如果消息内容不为空，
+		    // 说明该客户端想要提供
+		    // 新的消息保存在共享的 data 中，
+		    // 供别人获取
+		    data = e.data;
+		};
 	}, false);
 
 	port.start();
