086ae4f2bea927fd92affa86994f518806b897a3
吴成 <wubaodtc@sina.com>
2021-07-01 16:38:47 +0800

    Update index.js

--- sharedworker3/index.js
+++ sharedworker3/index.js
@@ -2,10 +2,17 @@ var worker = new SharedWorker("jsworker.js");
 
 function initJsWorker() {
 	worker.port.addEventListener("message", function (e) {
-		let divS = document.createElement("div");
-		divS.innerText = 'Waiting response: ' + e.data;
-		document.body.append(divS)
-		console.log('Waiting response: ' + e.data);
+		if(!e.data){
+			//表明没有人启动过SharedWorker
+			//启动SharedWorker，并广播通知
+			broadcastMessage()
+		} else {
+			//表面已经有人启动了SharedWorker
+			let divS = document.createElement("div");
+			divS.innerText = 'Waiting response: ' + e.data;
+			document.body.append(divS)
+			console.log('Waiting response: ' + e.data);
+		}
 	}, false);
 
 	worker.port.start();
@@ -13,10 +20,13 @@ function initJsWorker() {
 
 function sendMessage () {
 	var counter = 0;
-	
+	//1.先获取查看有没有启动了SharedWorker
+	worker.port.postMessage("getData");
+}
+
+function broadcastMessage(){
 	setInterval(function () {
-		counter++;
-		worker.port.postMessage("Hey sara!" + counter);
+		worker.port.postMessage("In the Meeting!");
 	}, 1000);
 }
 
