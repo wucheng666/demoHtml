af736970e9239888b0f9ef07847c4a88cc2e66d2
吴成 <wubaodtc@sina.com>
2021-07-06 15:02:02 +0800

    Update receive.js

--- sharedworker4meeting/receive.js
+++ sharedworker4meeting/receive.js
@@ -1,8 +1,17 @@
 var worker = new SharedWorker("jsworker.js");
+let sendMessages = "";
 
 function initJsWorker() {
 	worker.port.addEventListener("message", function (e) {
 		broadcastMessage(e.data)
+		if(!e.data){
+			//表明没有人启动过SharedWorker
+			console.log("启动 SharedWorker...")
+			sendMessages = "mainWorkder send: true"
+			worker.port.postMessage(sendMessages);
+		} else if(sendMessages !== e.data){
+			alert("当前已经存在会议界面！")
+		}
 	}, false);
 	worker.port.addEventListener("messageerror", (event) => {
 		console.error(`Error receiving message from worker: ${event}`);
@@ -14,17 +23,6 @@ function initJsWorker() {
 function sendMessage () {
 	//1.先获取查看有没有启动了SharedWorker
 	worker.port.postMessage("getData");
-}
-
-function broadcastMessage(msg){
-	if(msg){
-		console.log("已经存在 SharedWorker...")
-		alert("当前已经存在会议界面！")
-	} else {
-		//表明没有人启动过SharedWorker
-		console.log("启动 SharedWorker...")
-		worker.port.postMessage(true);
-	}
 }
 
 initJsWorker();
