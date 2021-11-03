719efde37aaafcbc86bab818e1dc45fed98df9c8
吴成 <wubaodtc@sina.com>
2021-11-03 10:33:54 +0800

    Add files via upload

---
+++ audio/js/main4context.js
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
+ */
+
+'use strict';
+
+// Put variables in global scope to make them available to the browser console.
+const audio = document.querySelector('audio');
+
+const constraints = window.constraints = {
+  audio: true,
+  video: false
+};
+
+function handleSuccess(stream) {
+  // const audioTracks = stream.getAudioTracks();
+  // console.log('Got stream with constraints:', constraints);
+  // console.log('Using audio device: ' + audioTracks[0].label);
+  // stream.oninactive = function() {
+  //   console.log('Stream ended');
+  // };
+  // window.stream = stream; // make variable available to browser console
+  // audio.srcObject = stream;
+
+  let audioContext = new AudioContext({ sampleRate: 16000 });
+  document.getElementById('tips').innerHTML = ac.sampleRate;
+
+  //创建音频处理的源节点
+  let source = audioContext.createMediaStreamSource(stream);
+  //创建自定义处理节点
+  let scriptNode = audioContext.createScriptProcessor(4096, 1, 1);
+  //创建音频处理的输出节点
+  let dest = audioContext.createMediaStreamDestination();
+
+  //串联连接
+  source.connect(scriptNode);
+  scriptNode.connect(dest);
+
+
+}
+
+function handleError(error) {
+  const errorMessage = 'navigator.MediaDevices.getUserMedia error: ' + error.message + ' ' + error.name;
+  document.getElementById('errorMsg').innerText = errorMessage;
+  console.log(errorMessage);
+}
+
+navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
